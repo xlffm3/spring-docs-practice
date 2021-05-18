398604eea808cd4e8dc81018079027aaa6cca232
Jinhong <xlffm3@gmail.com>
2021-05-18 14:45:14 +0900

    feat: first commit

---
+++ src/test/java/practice/docs/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.docs.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
